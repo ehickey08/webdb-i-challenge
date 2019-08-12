9ab8378091469fdf4ee35205d980c9f0060fda8d
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-08-12 15:56:38 -0400

    Create CRUD for server endpoints

--- server.js
+++ server.js
@@ -6,6 +6,76 @@ const server = express();
 
 server.use(express.json());
 
-module.exports = server;
+server.get('/accounts', (req, res) => {
+    db('accounts')
+        .then(accounts => {
+            res.status(200).json(accounts);
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Internal server error.' });
+        });
+});
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+server.get('/accounts/:id', (req, res) => {
+    db('accounts')
+        .where({ id: req.params.id })
+        .first()
+        .then(account => {
+            if (account) res.status(200).json(account);
+            else
+                res.status(404).json({
+                    message: 'An account with that ID does not exist. ',
+                });
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Internal server error.' });
+        });
+});
+
+server.post('/accounts', (req, res) => {
+    let newAccount = req.body;
+    db('accounts')
+        .insert(newAccount)
+        .then(id => {
+            res.status(201).json(id);
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Internal server error' });
+        });
+});
+
+server.delete('/accounts/:id', (req, res) => {
+    db('accounts')
+        .where({ id: req.params.id })
+        .delete()
+        .then(amt => {
+            if (amt)
+                res.status(200).json({ message: `${amt} record(s) deleted.` });
+            else
+                res.status(404).json({
+                    message: 'An account with that ID does not exist. ',
+                });
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Internal server error' });
+        });
+});
+
+server.put('/accounts/:id', (req, res) => {
+    const changes = req.body;
+    db('accounts')
+        .where({ id: req.params.id })
+        .update(changes)
+        .then(amt => {
+            if (amt)
+                res.status(200).json({ message: `${amt} record(s) updated.` });
+            else
+                res.status(404).json({
+                    message: 'An account with that ID does not exist. ',
+                });
+        })
+        .catch(() => {
+            res.status(500).json({ message: 'Internal server error' });
+        });
+});
+module.exports = server;
